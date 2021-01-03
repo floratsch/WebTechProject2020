897b79138c865f9b87d96a3b7a80440ecdd99bfb
floratsch <flratschnig@edu.aau.at>
2021-01-03 14:42:28 +0100

    server port updated

--- NodeJS & MySQL/node-mysql-registration-login/server.js
+++ NodeJS & MySQL/node-mysql-registration-login/server.js
@@ -12,8 +12,6 @@ app.use(cors());
 // api routes
 app.use('/users', require('./users/users.controller'));
 
-
-
 // start server
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+const port =  4000;
 app.listen(port, () => console.log('Server listening on port ' + port));
